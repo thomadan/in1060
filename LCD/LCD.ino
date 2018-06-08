d73e788a19d63341e1fcebb22d0d32e863cbef42
thomadan <thomas.dahl.andersen@gmail.com>
2018-06-08 12:11:08 +0200

    Add files via upload

---
+++ LCD/LCD.ino
@@ -0,0 +1,60 @@
+// kode for den arudinoen som styrer LCD
+
+// hent inn LCD-biblioteket som følger med arduinos IDE
+#include <LiquidCrystal.h>
+
+// sette pins til LCD (konstant fordi disse bør ikke kunne endres)
+const int rs = 12,          // switch for å sende enten kommando eller data til LCD (pin 4 på LCD)
+          en = 11,          // åpne for skriving til LCD (pin 6 på LCD)
+          d4 = 5,           // pins for å sende 8-bit data til LCD (bruker bare bakerste 4 bits) 
+          d5 = 4,
+          d6 = 3,
+          d7 = 2;
+
+// opprette instans av LCD-klassen med pins som parametere
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int ARDUINO_LINK = 6;             // pin for input fra RFID-boksen
+const int LED = 8;                      // pin for LED
+String kylling = "Tomt for kylling";    // tekst som skal vises på LCD
+
+// oppsett av det som skal skje før koden begynner å loope
+void setup() {
+  
+  // sette modus for LED-pin til ut
+  pinMode(LED, OUTPUT);               
+
+  // starte lcd-objektet, med parametere kolonner og rader av tegn
+  lcd.begin(16, 2);
+  
+  // skrive første beskjed til skjerm
+  lcd.print("Venter på beskjed fra RFID");
+}
+
+// denne metoden går i løkke så lenge systemet er på
+void loop() {
+
+  // hvis det kommer beskjed fra den andre arduinoen
+  if (digitalRead(ARDUINO_LINK)) {
+
+    // sette pekeren til hvor på LCD-en det skal skrives
+    lcd.setCursor(0, 0);
+
+    // skrive beskjed
+    lcd.print(kylling);
+
+    // skru på LED
+    digitalWrite(LED, HIGH);
+  }
+
+  // hvis det ikke er beskjed fra den andre arduinoen
+  else {
+
+    // viske ut beskjed på LCD
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
+
+    // skru av LED
+    digitalWrite(LED, LOW);
+  }
+}
---
+++ RFID/RFID.ino
@@ -0,0 +1,100 @@
+// kode for den delen av prototypen som tar imot RFID-brikker
+
+// importere RFID-biblioteket: https://github.com/miguelbalboa/rfid    
+#include <MFRC522.h> 
+  
+// importere bibliotekt for SPI-protokollen, som brukes av RFID      
+#include <SPI.h>                    
+
+const int SS_PIN = 10;                    // pin for RFID-leseren
+const int RST_PIN = 9;                    // pin for RFID-leseren
+const int ARDUINO_LINK = 3;               // pin for link-kabel mellom arduino-ene
+bool rfidStatus = false;                  // status for RFID 
+unsigned long brikkeSistGodkjent = 0;     // holde tiden på når brikken sist ble godkjent
+unsigned long brikkeSistRegistrert = 0;   // holde tiden på når brikken sist ble registrert
+int debounceForsinkelse = 500;            // hvor lenge det må gå mellom hver godkjenning
+String lestKode = "";                     // holder for koden som leses fra en RFID-brikke
+
+// opprette instans av MFRC522-klassen
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+// oppsett før loopen begynner
+void setup() {
+  SPI.begin();                            // starte SPI
+  mfrc522.PCD_Init();                     // klargjøre MFRC522
+  pinMode(ARDUINO_LINK, OUTPUT);          // setter link-pin til ut-modus
+}
+
+// koden som går igjen og igjen
+void loop() {
+ 
+  // se etter nye kort
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  
+  // lese av kort
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // få tak i koden til kortet, byte for byte
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+
+     // legge til tegn i strengen for avlest kode
+     // hvis byte er bare ett tegn, legg til en 0, slik at det blir to tall: "00"
+     lestKode.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+
+     // legge til tegn i lestKode, som HEX
+     lestKode.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+
+  // hvis det er kort 1, gi beskjed til den andre arduinoen
+  // begynne på 1, siden 0 er mellomrom
+  if (lestKode.substring(1) == "33 73 80 b9") {
+
+    // si fra at brikken ble registrert nå
+    brikkeSistRegistrert = millis();
+
+    // hvis det er lenge nok siden sist kortet ble godkjent
+    // slik at det ikke kommer mange godkjenninger selv om 
+    // bruker bare mener å trekke kortet en gang
+    if (sjekkDebounce() == true) {
+
+      // si fra at kortet er godkjent
+      rfidState = true;
+
+      // markere tiden kortet sist ble godkjent, for debounce
+      brikkeSistGodkjent = millis();
+
+      // sette link-pin til på, slik at den andre arduinoen får beskjed
+      digitalWrite(ARDUINO_LINK, HIGH);
+    }
+  }
+
+// hvis det er kort 2, si fra til den andre arduinoen at den skal skru av
+  else if (lestKode.substring(1) == "70 4d 92 58") {
+    
+    // si fra til den andre arduinoen at den skal skru av
+    digitalWrite(ARDUINO_LINK, LOW);
+
+    // markere at RFID er av
+    rfidState = 0;
+  }
+} 
+
+// sjekke om det har gått rimelig tid siden sist kortet ble godkjent
+bool sjekkDebounce() {
+
+  // hvis det har gått lenger enn debounce-tiden ..
+  if ((millis() - brikkeSistGodkjent) > debounceDelay) {
+
+    // .. og hvis RFID ikke allerede er på
+    if (rfidState == false) {
+      return true;
+    }
+  }
+
+  // hvis det ikke har gått lang nok tid siden sist, ikke godkjenn brikken
+  return false;
+}
